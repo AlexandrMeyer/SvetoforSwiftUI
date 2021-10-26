6fd9f378ca388ac52abccb2d4ec3cd7a1ccff1fc
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-10-26 11:15:17 +0300

    Initial Commit

---
+++ SvetoforSwiftUI/SvetoforSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SvetoforSwiftUIApp.swift
+//  SvetoforSwiftUI
+//
+//  Created by Александр on 26.10.21.
+//
+
+import SwiftUI
+
+@main
+struct SvetoforSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
